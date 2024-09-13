9f0c79c0d61b34082251945cdbc45e423c43a195
MC Foster <mariechantalfoster@gmail.com>
2024-09-13 16:33:38 -0400

    commit 1

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,8 @@
 const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
 for (let x = 0; x < topics.length; x++) {
-console.log(topics[x]);
+ console.log(topics[x]);
 }
-
 
 /*
 if (topic === 'HTML') {
